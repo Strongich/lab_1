05100bd18b4ac3ee9c58199a26854d4cc3016443
Strongich <igor.vitenko13@gmail.com>
2022-05-28 20:08:07 +0300

    Second commit

--- src/main/scala/List.scala
+++ src/main/scala/List.scala
@@ -1,50 +1,60 @@
 package com.tkroman.kpi.y2022.l1
-
 import scala.collection.mutable
 
 enum List[+A]:
   case Nil extends List[Nothing]
   case Cons(h: A, tl: List[A]) extends List[A]
 
-  def foldRight[B](z: B)(f: (A, B) => B): B = { // rdy
+  override def toString: String =
+    @scala.annotation.tailrec def go(sb: StringBuilder, as: List[A]): String = {
+      as match {
+        case Nil => sb.result
+        case Cons(h, t @ Nil) => go(sb.append(h).append("]"), t)
+        case Cons(h, t) => go(sb.append(h).append(", "), t)
+      }
+    }
+    go(new StringBuilder("["), this)
+
+  def foldRight[B](z: B)(f: (A, B) => B): B = {
     this match
       case Nil => z
-      case Cons(h: A, tl: List[A]) => f(h, foldRight(z)(f))
+      case Cons(xh, xt) => f(xh, xt.foldRight(z)(f))
   }
 
   def concat[A](xs: List[A], ys: List[A]): List[A] = {
-    // tail recursive
-    /*    xs match
-      case Nil => ys
-      case Cons(h: A, tl: List[A]) => concat(tl, ys) */
-    // foldRight
-    xs match
-      case Nil => ys
-      case Cons(xh,xt) => Cons(xs,Cons(ys,Nil)).flatMap(identity)
+    @scala.annotation.tailrec
+    def go[A](xs: List[A],acc: List[A]): List[A] = {
+      xs match
+        case Nil => acc
+        case Cons(xh: A, xt: List[A]) => go(xt, Cons(xh, acc))
+    }
+    go(xs.reverse(xs),ys)
+
+ /*   xs match
+     case Nil => ys
+     case Cons(xh,xt) => Cons(xs,Cons(ys,Nil)).flatMap(identity)
+*/
   }
 
   def flatMap[B](f: A => List[B]): List[B] = {
-    // tail recursive
-   /*  this match
-      case Nil => Nil
-      case Cons(h: A, tl: List[A]) => tl.flatMap(f) */
-
-    // fold right
-    def go[B](xs: List[A], acc: List[B], f: A => List[B]): List[B] = acc match {
-      case Nil => acc
-      case Cons(xh: A,xt: List[A]) => xs.foldRight(acc)((xh,acc)=>f(xh))
+    /* this match {
+       case Nil => Nil
+       case Cons(xh: B, xt: List[A]) => concat(f(xh), xt.flatMap(f))
+     }
+ */
+      this match
+        case Nil => Nil
+     //   case Cons(xh,xt) => foldRight(f(xh))((xh,)
     }
-    go(Nil,Nil,f)
-  }
 
-  def zip[B](ys: List[B]): List[(A,B)] =
+  def zip[B](ys: List[B]):List[(A,B)] =
     def go(xs: List[A], ys: List[B], acc: List[(A, B)]): List[(A, B)] = (xs, ys) match {
-      case (Nil, _) | (_, Nil) => acc
-      case (Cons(xh, xt), Cons(yh, yt)) =>
+      case (Nil, _) | (_, Nil) => acc.reverse(acc)
+      case (Cons(xh, xt:List[A]), Cons(yh, yt:List[A])) =>
       val d = (xh, yh)
       go(xt, yt, Cons(d, acc))
  }
-    go(Nil, ys, Nil)
+    go(this,ys,Nil)
 
   def partition[A](xs: List[A])(pred: A => Boolean): (List[A],List[A]) = {
     def togo(xs: List[A])(left: List[A], right: List[A]): (List[A], List[A]) = xs match {
@@ -57,20 +67,18 @@ enum List[+A]:
     togo(xs)(Nil,Nil)
 }
   def reverse[A](xs: List[A]): List[A] =
-    def togo_1(xs:List[A], acc: List[A]): List[A] = xs match {
+    def togo_1(xs: List[A], acc: List[A]): List[A] = xs match {
       case Nil => acc
-      case Cons(xh,xt) => togo_1(Cons(xh,acc),xt)
+      case Cons(xh,xt) => togo_1(xt,Cons(xh,acc))
   }
     togo_1(xs,Nil)
 
+@main def run = {
+  import List.*
+  println("Hello")
+  val xs = Cons(1,Cons(2,Cons(3,Nil)))
+  println(xs.flatMap(xs)(x => Cons(x.toString,Cons(x.toString,Cons(x.toString,Nil)))))
 
-// NOTE: do not use this to demo the results.
-// Use unit-tests instead
-@main def run() = {
-println("Hello")
-
-
-
-
-
+ val xs1 = Seq(1,2,3)
+  println(xs1.flatMap(x=> Seq(x.toString,x.toString,x.toString)))
 }
--- src/test/scala/ListSuite.scala
+++ src/test/scala/ListSuite.scala
@@ -3,36 +3,42 @@ package com.tkroman.kpi.y2022.l1
 import scala.collection.mutable
 import munit.FunSuite
 
+import List.*
+
 class ListSuite extends FunSuite {
   test("foldRight") {
     val expected = -94
-    val actual = mutable.Buffer(1, 3, 8).foldRight(100)(_ - _)
+    val xs = Cons(1,Cons(3,Cons(8,Nil)))
+    val actual = xs.foldRight(100)(_-_)
     assertEquals(expected, actual)
   }
+
   test("concat") {
-    val expected = mutable.ArrayBuffer('a', 'b', 'c', 'd', 'f')
-    var ys = mutable.ArrayBuffer('d', 'f')
-    var xs = mutable.ArrayBuffer('a', 'b', 'c')
-    val actual = xs.concat(ys)
+    val expected = Cons("a",Cons("b",Cons("c",Cons("d",Cons("f",Nil)))))
+    val ys = Cons("d",Cons("f",Nil))
+    val xs = Cons("a",Cons("b",Cons("c",Nil)))
+    val actual = xs.concat(xs,ys)
     assertEquals(expected, actual)
   }
+
   test("flatmap") {
-    val expected = Vector('A', 'B', 'C', 'D', 'E', 'F', 'G', 'T', 'H')
-    val a = Vector("abcde", "fgth")
-    val actual = a.flatMap(_.toUpperCase)
+    val expected = Cons("1",Cons("1",Cons("1",Cons("2",Cons("2",Cons("2",Cons("3",Cons("3",Cons("3",Nil)))))))))
+    val a = Cons(1, Cons(2, Cons(3,Nil)))
+    val actual = a.flatMap(a)(x=> Cons(x.toString,Cons(x.toString,Cons(x.toString,Nil))))
     assertEquals(expected, actual)
   }
+
   test("zip") {
-    val expected = mutable.Buffer((1,'a'), (2,'b'), (3,'c'))
-    val xs = mutable.Buffer(1,2,3)
-    val ys = mutable.Buffer('a','b','c','d')
+    val expected = Cons((1,'a'),Cons((2,'b'),Cons((3,'c'),Nil)))
+    val xs = Cons(1,Cons(2,Cons(3,Nil)))
+    val ys = Cons('a',Cons('b',Cons('c',Cons('d',Nil))))
     val actual = xs.zip(ys)
     assertEquals(expected, actual)
   }
   test("partition") {
-    val expected = (mutable.Buffer(3, 6),mutable.Buffer(1, 2, 4, 5, 7))
-    val xs = mutable.Buffer(1,2,3,4,5,6,7)
-    val actual = xs.partition(x => x%3 == 0)
+    val expected = (Cons(3,Cons(6,Nil)),Cons(1,Cons(2,Cons(4,Cons(5,Cons(7,Nil))))))
+    val xs = Cons(1,Cons(2,Cons(3,Cons(4,Cons(5,Cons(6,Cons(7,Nil)))))))
+    val actual = xs.partition(xs)(x => x%3 == 0)
     assertEquals(expected,actual)
   }
 }
